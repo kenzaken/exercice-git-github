4829a705f4b96d70e70995a23313c95abd04af84
kenzaken <kenza01ken12@gmail.com>
2026-04-01 15:31:10 +0200

    new push

--- tests/exercice.spec.ts
+++ tests/exercice.spec.ts
@@ -7,7 +7,8 @@ import { link } from 'node:fs'
 //remplir les champs  
 // ensuite cliquer sur le button valide 
 
-test("un test qui ne permet de s'inscrire sur le site https://agropeyi.fr/", async ({ page }) => {
+
+test("un test qui ne permet de s'inscrire sur le site https://agropeyi.fr/",{tag:"@regression"}, async ({ page }) => {
     //email random
     let number = Math.floor(Math.random() * 200) + 10
     let email = "kenza" + number + "@gmail.com"
